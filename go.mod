d95940f652b9edcbbc00e46b6c1299b100f62842
patipancorb <162946824+patipancorb@users.noreply.github.com>
2025-05-30 10:09:31 +0700

    u m b

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/popkakkk/my-module-b
 
 go 1.24.3
+
+require github.com/popkakkk/my-module-c v1.7.0
--- util.go
+++ util.go
@@ -1,5 +1,12 @@
 package b_package
 
+import (
+	"fmt"
+
+	c "github.com/popkakkk/my-module-c" // Adjust the import path as necessary
+)
+
 func UtilFunction() string {
+	fmt.Println("use package c  --> ", c.UtilFunction())
 	return "This is a utility function from package b"
 }
